58e4527b1004360777dd99707510b34c1388f897
minhviet21 <vmdiioenathn@gmail.com>
2024-04-17 09:05:46 +0700

    update delete on pro com

--- myproject/views.py
+++ myproject/views.py
@@ -5,11 +5,6 @@ from .models import ProductComponent
 def homepage(request):
     return render(request, "myproject/homepage.html")
 
-def index(request):
-    list_product = ProductComponent.objects.all()
-    context = {"list_product": list_product}
-    return render(request, "myproject/index.html", context)
-
 def productcomponent(request):
     list_pro_com = ProductComponent.objects.all()
     context = {"list_pro_com": list_pro_com}
@@ -19,8 +14,11 @@ def updateproductcomponent(request, id):
     pro_com = get_object_or_404(ProductComponent, id=id)
     if request.method == 'POST':
         quantity = request.POST.get('quantity')
-        pro_com.quantity = quantity
-        pro_com.save()
+        if quantity == "0":
+            pro_com.delete()
+        else:
+            pro_com.quantity = quantity
+            pro_com.save()
         return redirect('productcomponent')
     return render(request, "myproject/updateproductcomponent.html", {'pro_com': pro_com})
 
